9bb43bac9d5f0d831a2c2dcd3d3246d4825e3ca5
Caty <catymaepaints@gmail.com>
2024-06-08 23:31:12 -0400

    adjusted contact button size

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PortfolioImage from '../images/portfolioPhoto.png'
 import './Footer.css';
 
 const Footer = () => {
   return (
     <footer>
-      <h1 class-name="">Footer</h1>
+      <div className="footer-content">
+        <div className="logo">
+            <img src={PortfolioImage} alt="Logo" />
+          </div>
+        <div className="social-media">
+          <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+          <a href="https://github.com" target="_blank" rel="noopener noreferrer">GitHub</a>
+          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a>
+        </div>
+      </div>
+      <p>© 2024 Your Company. All rights reserved.</p>
     </footer>
   );
 };
