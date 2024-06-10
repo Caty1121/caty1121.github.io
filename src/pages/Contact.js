be1e79c000f318c26c9d394e6dd055f72bc4e1d9
Caty <catymaepaints@gmail.com>
2024-06-09 23:00:18 -0400

    updated footer

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,14 +4,20 @@ import './Contact.css';
 const Contact = () => {
   return (
     <section id="contact">
-      <h2>Contact</h2>
+      <h2>Get in Touch.</h2>
       <form action="submit_form.php" method="post">
-        <label for="name">Name</label>
-        <input type="text" id="name" name="name" />
-        <label for="email">Email</label>
-        <input type="email" id="email" name="email" />
-        <label for="message">Message</label>
-        <textarea id="message" name="message"></textarea>
+        <div class="form-right">
+          <label for="name"></label>
+          <input type="text" id="name" name="name" placeholder="Name"/>
+          <label for="email"></label>
+          <input type="email" id="email" name="email" placeholder="email"/>
+          <label for="phone"></label>
+          <input type="phone" id="phone" name="phone" placeholder="Phone Number"/>
+        </div>
+        <div class="form-left"> 
+          <label for="message"></label>
+          <textarea id="message" name="message" placeholder="Message"></textarea>
+          </div>
         <button type="submit">Send</button>
       </form>
     </section>
