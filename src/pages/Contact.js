1717b9272251267c904232c34555135f33f14d64
Caty <catymaepaints@gmail.com>
2024-06-10 21:57:10 -0400

    fixed up about and contact pages

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,21 +5,23 @@ const Contact = () => {
   return (
     <section id="contact">
       <h2>Get in Touch.</h2>
-      <form action="submit_form.php" method="post">
-        <div class="form-right">
-          <label for="name"></label>
-          <input type="text" id="name" name="name" placeholder="Name"/>
-          <label for="email"></label>
-          <input type="email" id="email" name="email" placeholder="email"/>
-          <label for="phone"></label>
-          <input type="phone" id="phone" name="phone" placeholder="Phone Number"/>
-        </div>
-        <div class="form-left"> 
-          <label for="message"></label>
-          <textarea id="message" name="message" placeholder="Message"></textarea>
-          </div>
+      <div className="form-box">
         <button type="submit">Send</button>
-      </form>
+        <form action="submit_form.php" method="post">
+          <div class="form-right">
+            <label for="name"></label>
+            <input type="text" id="name" name="name" placeholder="Name"/>
+            <label for="email"></label>
+            <input type="email" id="email" name="email" placeholder="email"/>
+            <label for="phone"></label>
+            <input type="phone" id="phone" name="phone" placeholder="Phone Number"/>
+          </div>
+          <div class="form-left"> 
+          <label for="message"></label>
+          <textarea id="message" name="message" placeholder="Message" Class="message"></textarea>
+          </div>  
+        </form>
+      </div>
     </section>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './Home.css';
+import portfolioPhotoImage from '../images/portfolioPhoto.png';
 
 const Home = () => {
   return (
     <div className="home-container">
+      <div className="container">
+        <img src={portfolioPhotoImage} alt="portfolioPhoto" class="portfolioPhotoImage"/>
+      </div>
       <h2 className="home-title">Hi, I'm Caty.</h2>
       <p className="home-description">This is the home page.</p>
     </div>
