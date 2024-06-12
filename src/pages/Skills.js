d96da540353aa7f03254c060f13e7093e80e91e0
Caty <catymaepaints@gmail.com>
2024-06-12 10:31:19 -0400

    added bootstrap to skills page

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { ProgressBar } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './Skills.css';
 
 const Skills = () => {
@@ -7,20 +9,46 @@ const Skills = () => {
       <h2>Skills</h2>
       <ul>
         <li>
-          <h3>HTML, CSS, JavaScript</h3>
-          <p>Experience building responsive websites with modern web standards.</p>
+          <h3>HTML</h3>
+          <div className="progress-container">
+            <ProgressBar now={100} striped label="" variant="info"/>
+          </div>
+        </li>
+        <li>
+          <h3>CSS</h3>
+          <div className="progress-container">
+            <ProgressBar now={85} striped label="" variant="primary"/>
+          </div>
+        </li>
+        <li>
+          <h3>JavaScript</h3>
+          <div className="progress-container">
+            <ProgressBar now={75} striped label="" variant="danger"/>
+          </div>
+        </li>
+        <li>
+          <h3>Python</h3>
+          <div className="progress-container">
+            <ProgressBar now={80} striped label="" variant="warning"/>
+          </div>
+        </li>
+        <li>
+          <h3>SQL</h3>
+          <div className="progress-container">
+            <ProgressBar now={70} striped label="" variant="dark"/>
+          </div>
         </li>
         <li>
           <h3>React</h3>
-          <p>Proficient in building dynamic web applications using React.</p>
+          <div className="progress-container">
+            <ProgressBar now={70} striped label="" variant="info"/>
+          </div>
         </li>
         <li>
           <h3>Django</h3>
-          <p>Experience developing backend services and APIs using Django.</p>
-        </li>
-        <li>
-          <h3>Python, SQL</h3>
-          <p>Skilled in writing efficient code for data processing and database management.</p>
+          <div className="progress-container">
+            <ProgressBar now={60} striped label="" variant="success"/>
+          </div>
         </li>
       </ul>
     </section>
