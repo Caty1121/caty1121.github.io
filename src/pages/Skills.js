e290720194dd4bde5e1e38e2fbf9534dc5f50aeb
Caty <catymaepaints@gmail.com>
2024-06-13 07:03:55 -0400

    final edits

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -11,13 +11,13 @@ const Skills = () => {
         <section class="intro">
           <h3>Introduction</h3>
             <p>
-                Caty Mae is a skilled programmer and web developer with a diverse background in both technology and athletics. With an Associate of Arts (AA) degree in computer programming and currently pursuing a Bachelor of Science (BS) degree in web development, Caty has a strong foundation in computer science and programming principles.
+                Caty Mae is a skilled programmer and web developer with a diverse background in technology and mentorship. With a degree in computer programming and web development, Caty has a strong foundation in computer science and programming principles.
             </p>  
             <p>
-              Caty's professional journey includes significant experience as a programmer, specializing in SAP CPQ products. Holding an SAP certification, Caty has demonstrated expertise in developing and managing complex software solutions. Her role at Aspire Digital involves coordinating production support ticketing triage efforts, enhancing security within the codebase by identifying and replacing vulnerable queries, and creating efficient processes for managing changes.
+              Caty's professional journey includes significant experience with e-commerce develoment and design and SAAS products. Holding an SAP CPQ certification, Caty has demonstrated expertise in developing and managing complex software solutions. Her current role at involves leading daily scrum calls, front and back end development, and process improvement.
             </p>
             <p>
-              In addition to her technical skills, Caty has a passion for helping others. This is reflected in her transition from being a coach to becoming a programmer who actively assists colleagues in the workplace. She is committed to continuous learning and improvement, tackling Python practice problems one step at a time to build her expertise.
+              In addition to her technical skills, Caty has a passion for helping others. This is reflected in her transition from being a coach to becoming a programmer who actively assists colleagues in the workplace. She is committed to continuous learning and improvement, tackling one project at a time to continue to build her expertise.
             </p>
         </section>
         <section class="tech-stack">
@@ -73,25 +73,25 @@ const Skills = () => {
               Tool Box
             </h3>
               <li>
-                <h4>Bootstrap</h4>
+                <h4 class="tool-box-content">Bootstrap</h4>
               </li>
               <li>
-                <h4>VS Code</h4>
+                <h4 class="tool-box-content">VS Code</h4>
               </li>
               <li>
-                <h4>Postman</h4>
+                <h4 class="tool-box-content">Postman</h4>
               </li>
               <li>
-                <h4>Figma | Penpot</h4>
+                <h4 class="tool-box-content">Figma | Penpot</h4>
               </li>
               <li>
-                <h4>Git | Github</h4>
+                <h4 class="tool-box-content">Git | Github</h4>
               </li>
               <li>
-                <h4>Bitbucket | Jenkins</h4>
+                <h4 class="tool-box-content">Bitbucket | Jenkins</h4>
               </li>
               <li>
-                <h4>Jira | ServiceNow</h4>
+                <h4 class="tool-box-content">Jira | ServiceNow</h4>
               </li>
           </ul>
         </section>  
